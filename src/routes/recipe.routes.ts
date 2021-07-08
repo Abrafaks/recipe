ff0bb7f42b1b1c6ddbd1d8c68aceb01d0b3c97b7
Marcin Matoga <marcin.matoga.poznan@gmail.com>
2021-07-08 13:43:57 +0200

    Init commit

--- src/routes/recipe.routes.ts
+++ src/routes/recipe.routes.ts
@@ -4,16 +4,14 @@ import * as recipeController from "../controllers/recipe.controller";
 
 const router = express.Router();
 
-router.post("/create", auth, recipeController.create);
+router.post("/", auth, recipeController.create);
 
-router.post("/readAll", auth, recipeController.readAll);
+router.get("/", auth, recipeController.readAll);
 
-router.post("/read", auth, recipeController.read);
+router.get("/:id", auth, recipeController.read);
 
-// Shouldn't it be changed to put?
-router.post("/update", auth, recipeController.update);
+router.get("/readByName", auth, recipeController.readByName);
 
-// And this will be changed to get
-router.post("/readByName", auth, recipeController.readByName);
+router.put("/", auth, recipeController.update);
 
 export default router;
