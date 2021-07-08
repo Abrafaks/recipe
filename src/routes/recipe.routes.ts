4a5e864a128b7701d06c19f3b9aaa423adbef5c2
Marcin Matoga <marcin.matoga.poznan@gmail.com>
2021-07-08 10:32:17 +0200

    Use classes in user services, implement recipe finding

--- src/routes/recipe.routes.ts
+++ src/routes/recipe.routes.ts
@@ -1,16 +1,16 @@
 import express from "express";
 import auth from "./middleware/auth";
-import * as recipe_controller from "../controllers/recipe.controller";
+import * as recipeController from "../controllers/recipe.controller";
 
 const router = express.Router();
 
-router.post("/create", auth, recipe_controller.create);
-router.post("/read", auth, recipe_controller.read);
+router.post("/create", auth, recipeController.create);
+router.post("/read", auth, recipeController.read);
 
 // Shouldn't it be changed to put?
-router.post("/update", auth, recipe_controller.update);
+router.post("/update", auth, recipeController.update);
 
 // And this will be changed to get
-// router.post("/readByName", auth, recipe_controller.readByName);
+router.post("/readByName", auth, recipeController.readByName);
 
 export default router;
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -3,58 +3,65 @@ import jwt from "jsonwebtoken";
 import { Response } from "express";
 import { User } from "../models/user.model";
 
-export function setCookie(res: Response, token: string): Response {
-  return res
-    .cookie("token", token, {
-      httpOnly: true,
-    })
-    .send();
+export class UserService {
+  public setCookie(res: Response, token: string): Response {
+    return res
+      .cookie("token", token, {
+        httpOnly: true,
+      })
+      .send();
+  }
+
+  public unsetCookie(res: Response): Response {
+    return res
+      .cookie("token", "", {
+        httpOnly: true,
+        expires: new Date(0),
+      })
+      .send();
+  }
+
+  // user will be null if there isn't
+  // this email in db => account can be registered
+  public async getUserByEmail(email: string): Promise<User | null> {
+    return await User.findOne({ email });
+  }
+
+  public async hashPassword(password: string, salt: number): Promise<string> {
+    return await bcrypt.hash(password, salt);
+  }
+
+  public async createUser(email: string, passwordHash: string): Promise<User> {
+    const user = new User({ email, passwordHash });
+
+    return await user.save();
+  }
+
+  public createToken(userId: string, isAdmin: boolean): string {
+    return jwt.sign(
+      {
+        userId,
+        isAdmin,
+      },
+      process.env.JWT!
+    );
+  }
+
+  public async createUserAndReturnToken(
+    email: string,
+    password: string
+  ): Promise<string> {
+    const passwordHash = await this.hashPassword(password, 10);
+    const savedUser = await this.createUser(email, passwordHash);
+    return this.createToken(savedUser._id, false);
+  }
+
+  public async arePasswordsMatching(
+    password: string,
+    hash: string
+  ): Promise<boolean> {
+    return await bcrypt.compare(password, hash);
+  }
 }
 
-export function unsetCookie(res: Response): Response {
-  return res
-    .cookie("token", "", {
-      httpOnly: true,
-      expires: new Date(0),
-    })
-    .send();
-}
-
-// user will be null if there isn't
-// this email in db => account can be registered
-export async function getUserByEmail(email: string): Promise<User | null> {
-  return await User.findOne({ email });
-}
-
-export async function hashPassword(
-  password: string,
-  salt: number
-): Promise<string> {
-  return await bcrypt.hash(password, salt);
-}
-
-export async function createUser(
-  email: string,
-  passwordHash: string
-): Promise<User> {
-  const user = new User({ email, passwordHash });
-
-  return await user.save();
-}
-
-export function createToken(userId: string, isAdmin: boolean): string {
-  return jwt.sign(
-    {
-      userId,
-      isAdmin,
-    },
-    process.env.JWT!
-  );
-}
-
-export async function arePasswordsMatching(
-  password: string,
-  hash: string
-): Promise<boolean> {
-  return await bcrypt.compare(password, hash);
-}
+export default new UserService();
