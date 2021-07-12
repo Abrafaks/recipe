b068dd00b1a56af40199731bf5af7cc51fd1233d
Marcin Matoga <marcin.matoga.poznan@gmail.com>
2021-07-12 23:25:53 +0200

    Change folder structure

--- src/server.ts
+++ src/server.ts
@@ -1,37 +1,5 @@
-import express from "express";
-import mongoose from "mongoose";
-import cookieParser from "cookie-parser";
-import dotenv from "dotenv";
-import userRouter from "./routes/user.routes";
-import recipeRouter from "./routes/recipe.routes";
+import app from "./app";
 
-dotenv.config();
-const app = express();
-
-const PORT = process.env.PORT || 3000;
-
-app.use(express.json());
-app.use(cookieParser());
-
-mongoose.connect(
-  process.env.MONGODB_CONNECTION_STRING!,
-  {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  },
-  (err) => {
-    if (err) throw err;
-    console.log("MongoDB connection established");
-  }
-);
-
-//setting up routes
-
-app.use("/auth", userRouter);
-app.use("/recipe", recipeRouter);
-
-app.listen(PORT, () => {
-  console.log(`Server is up on port ${PORT}`);
+app.listen(app.get("port"), () => {
+  console.log(`Server is up on port ${app.get("port")}`);
 });
