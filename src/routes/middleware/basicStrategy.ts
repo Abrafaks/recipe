369b4cdfc70a20aa746e86f0bca6f3ff87fdcefc
Marcin Matoga <marcin.matoga.poznan@gmail.com>
2021-07-14 21:02:53 +0200

    Remove comments

--- src/routes/middleware/basicStrategy.ts
+++ src/routes/middleware/basicStrategy.ts
@@ -4,28 +4,20 @@ import userService from "../../services/user.service";
 
 const basicStrategy = new BasicStrategy(async (email, password, done) => {
   try {
-    console.log("start", email, password);
     const user = await User.findOne({ email });
     if (user) {
-      console.log("There is a user", user);
-
       const passwordCorrect = await userService.arePasswordsMatching(
         password,
         user.passwordHash
       );
-
       if (!passwordCorrect) {
         throw new Error("Invalid credentials.");
       }
-
       done(null, user);
     } else {
-      console.log("There is no user");
-
       done(null, false);
     }
   } catch (err) {
-    console.log("BS ERROR ", err);
     return done(err, null);
   }
 });
