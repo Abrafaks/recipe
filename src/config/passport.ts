2232f7d8bcea54fc8ca6bf50a452bc549f07ff4c
Marcin Matoga <marcin.matoga.poznan@gmail.com>
2021-07-14 09:53:23 +0200

    Remove comment

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -2,8 +2,6 @@ import passport from "passport";
 import { Strategy, ExtractJwt } from "passport-jwt";
 import { User } from "../models/user.model";
 
-// UserDocument is raw object from mongodb
-
 const options = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
   secretOrKey: process.env.JWT!,
