982eaab9efc0f5ff14b1018bbe1531cbdb01b7cc
Marcin Matoga <marcin.matoga.poznan@gmail.com>
2021-07-15 10:55:39 +0200

    Remove comments

--- src/@types/express/index.d.ts
+++ src/@types/express/index.d.ts
@@ -1,14 +1,4 @@
 import { UserDocument } from "../../models/user.model";
-
-// declare global {
-//   namespace Express {
-//     // eslint-disable-next-line @typescript-eslint/no-empty-interface
-//     interface User extends UserDocument {}
-//   }
-// }
-
-// interface ExpressUser extends Express.User, UserDocument {}
-
 declare global {
   namespace Express {
     interface User extends UserDocument {}
