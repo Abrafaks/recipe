1b8c2a018b533fc1ad45e363d086f0c802d30b67
Marcin Matoga <marcin.matoga.poznan@gmail.com>
2021-07-16 13:17:54 +0200

    Extended validation

--- src/routes/middleware/validators/recipe.validator.ts
+++ src/routes/middleware/validators/recipe.validator.ts
@@ -1,13 +1,36 @@
-import { body, ValidationChain } from "express-validator";
+import { body, ValidationChain, CustomValidator } from "express-validator";
 
-const title = body("title").notEmpty();
-const description = body("description").notEmpty();
-const url = body("url").notEmpty();
-const id = body("id").notEmpty();
+const title = body("title")
+  .notEmpty()
+  .isAlphanumeric("en-US", { ignore: " " })
+  .isLength({ min: 16, max: 80 })
+  .optional()
+  .exists();
+
+const description = body("description")
+  .notEmpty()
+  .isAscii()
+  .isLength({ min: 32, max: 256 });
+
+const url = body("url")
+  .notEmpty()
+  .matches(/(\/\w+)+\.\w+/i);
+
+const id = body("id").notEmpty().isMongoId();
 const preparing = body("preparing").isArray();
+const preparingContent = body(["preparing[*]"]).notEmpty().isAscii();
 const ingredients = body(["ingredients", "ingredients[*]"]).isArray();
+const ingredientsContent = body(["ingredients[*][*]"]).notEmpty().isAscii();
 
-const validateData = [title, description, preparing, ingredients, url];
+const validateData = [
+  title,
+  description,
+  preparing,
+  preparingContent,
+  ingredientsContent,
+  ingredients,
+  url,
+];
 
 export class RecipeValidator {
   public validateCreateRecipeData(): ValidationChain[] {
