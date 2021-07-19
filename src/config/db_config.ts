bc49e2f4ef64b049c605fbc518fcbfec507bfd70
Marcin Matoga <marcin.matoga.poznan@gmail.com>
2021-07-19 10:36:05 +0200

    Configure eslint

--- src/config/db_config.ts
+++ src/config/db_config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import mongoose from "mongoose";
 
 mongoose.connect(
--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { Request, Response } from "express";
 import userService, { UserService } from "../services/user.service";
 
