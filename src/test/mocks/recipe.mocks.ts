412618f32c3e8dc2a952c3d57c5c41adb43ff56d
Marcin Matoga <marcin.matoga.poznan@gmail.com>
2021-07-20 12:39:14 +0200

    Add recipe test

--- src/test/mocks/recipe.mocks.ts
+++ src/test/mocks/recipe.mocks.ts
@@ -22,8 +22,32 @@ const recipe = {
   url: "/this/will/be.changed",
 };
 
+const recipeWithInvalidData = {
+  title: 1,
+  description: [faker.lorem.sentences(3), "haha"],
+  preparing: [
+    faker.lorem.sentence(),
+    faker.lorem.sentence(),
+    2,
+    faker.lorem.sentence(),
+    faker.lorem.sentence(),
+  ],
+  ingredients: [
+    [faker.lorem.word(), faker.lorem.word()],
+    [faker.lorem.word(), faker.lorem.word()],
+    [faker.lorem.word(), faker.lorem.word()],
+    [faker.lorem.word(), faker.lorem.word()],
+    "Lorem ipsum",
+    [(faker.lorem.word(), faker.lorem.word())],
+    [faker.lorem.word(), faker.lorem.word()],
+    [faker.lorem.word(), faker.lorem.word()],
+    [faker.lorem.word(), faker.lorem.word()],
+  ],
+  url: "this/will/be.changed",
+};
+
 const deleteAllRecipes = function () {
   return Recipe.deleteMany();
 };
 
-export { recipe, deleteAllRecipes };
+export { recipe, deleteAllRecipes, recipeWithInvalidData };
