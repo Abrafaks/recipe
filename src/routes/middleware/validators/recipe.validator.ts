2a4609e067a2aa1aa9f2f07728ab404751d7ed02
Marcin Matoga <marcin.matoga.poznan@gmail.com>
2021-07-20 12:54:38 +0200

    Add custom validation messages

--- src/routes/middleware/validators/recipe.validator.ts
+++ src/routes/middleware/validators/recipe.validator.ts
@@ -1,30 +1,72 @@
 import { body, param, ValidationChain, query } from "express-validator";
 
-const title = body("title").notEmpty().isString().isLength({ min: 3, max: 80 });
+const title = body("title")
+  .notEmpty()
+  .withMessage("Title must not be empty")
+  .isString()
+  .withMessage("Title must be of type string")
+  .isLength({ min: 3, max: 80 })
+  .withMessage("Title's length must be between 3 and 80");
 
 const description = body("description")
   .notEmpty()
+  .withMessage("Description must not be empty")
   .isString()
+  .withMessage("Description must be of type string")
   .isLength({ max: 256 })
+  .withMessage("Description length must be max 256")
   .optional();
 
 const url = body("url")
   .notEmpty()
-  .matches(/^(\/\w+)+\.\w+$/i);
+  .withMessage("Url must not be empty")
+  .matches(/^(\/\w+)+\.\w+$/i)
+  .withMessage("Must be valid url");
 
-const id = body("id").notEmpty().isMongoId();
-const preparing = body("preparing").isArray();
-const preparingContent = body(["preparing[*]"]).notEmpty().isString();
-const ingredients = body(["ingredients", "ingredients[*]"]).isArray();
-const ingredientsContent = body(["ingredients[*][*]"]).notEmpty().isString();
+const id = body("id")
+  .notEmpty()
+  .withMessage("Id must not be empty")
+  .isMongoId()
+  .withMessage("Id must be valid mongodb id");
 
-const readId = param("id").notEmpty().isMongoId();
+const preparing = body("preparing")
+  .isArray()
+  .withMessage("Preparing must be array");
+
+const preparingContent = body(["preparing[*]"])
+  .notEmpty()
+  .withMessage("Preparing must not be empty")
+  .isString()
+  .withMessage("Preparing must be of type string");
+
+const ingredients = body(["ingredients", "ingredients[*]"])
+  .isArray()
+  .withMessage("Ingredients must be array");
+
+const ingredientsContent = body(["ingredients[*][*]"])
+  .notEmpty()
+  .withMessage("Ingredients must not be empty")
+  .isString()
+  .withMessage("Ingredients must be of type string");
+
+const readId = param("id")
+  .notEmpty()
+  .withMessage("ID must not be empty")
+  .isMongoId()
+  .withMessage("Id must be valid mongodb id");
 
 const paginationData = query(["skip", "limit"])
   .isInt({ min: 0 })
+  .withMessage("Pagination data must be positive integer")
   .toInt()
   .optional();
-const searchName = query("name").notEmpty().isString().optional();
+
+const searchName = query("name")
+  .notEmpty()
+  .withMessage("Name must not be empty")
+  .isString()
+  .withMessage("Name must be of type string")
+  .optional();
 
 const validateData = [
   title,
--- src/routes/middleware/validators/user.validator.ts
+++ src/routes/middleware/validators/user.validator.ts
@@ -1,7 +1,11 @@
 import { body, ValidationChain } from "express-validator";
 
-const email = body("email").isEmail();
-const password = body("password").isStrongPassword();
+const email = body("email").isEmail().withMessage("Invalid email");
+const password = body("password")
+  .isStrongPassword()
+  .withMessage(
+    "Password must have minimum length of 8 and contain at least 1 lowercase, 1 uppercase character, 1 symbol and 1 number"
+  );
 
 export class UserValidator {
   public validateUser(): ValidationChain[] {
