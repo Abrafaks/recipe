4fd5d8ace451c46c6e0b13eed5210f407c26ed02
Marcin Matoga <marcin.matoga.poznan@gmail.com>
2021-07-20 11:01:11 +0200

    Expand user testing

---
+++ src/test/recipe.spec.ts
@@ -0,0 +1,17 @@
+// import chai, { expect } from "chai";
+// import { server, close, user, getToken } from "./config/server.config";
+
+// before("Starting server", function(){
+// server()
+// })
+
+// after("Close database connection", async function () {
+//   await close();
+// });
+
+// describe("Recipe testing", function () {
+//   it("Should return token", function (done) {
+//     console.log(getToken());
+//     done();
+//   });
+// });
