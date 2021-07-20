aeae19fe962a30205b84f959acd4494d83c487f6
Marcin Matoga <marcin.matoga.poznan@gmail.com>
2021-07-20 12:39:14 +0200

    Add recipe test

--- src/test/recipe.spec.ts
+++ src/test/recipe.spec.ts
@@ -1,5 +1,9 @@
 import { chai, expect, app, StatusCodes } from "./config/server.config";
-import { recipe, deleteAllRecipes } from "./mocks/recipe.mocks";
+import {
+  recipe,
+  deleteAllRecipes,
+  recipeWithInvalidData,
+} from "./mocks/recipe.mocks";
 import { getToken } from "./mocks/user.mocks";
 
 let token = "Bearer ";
@@ -16,6 +20,7 @@ describe("Recipe testing", function () {
   it("should return token", function () {
     expect(token).to.be.a("string");
   });
+
   it("should create recipe", async function () {
     const response = await chai
       .request(app)
@@ -26,5 +31,19 @@ describe("Recipe testing", function () {
         ...recipe,
       });
     expect(response).to.have.status(StatusCodes.OK);
+  });
+
+  it("should not create recipe with invalid data", async function () {
+    let response = await chai
+      .request(app)
+      .post("/recipe/")
+      .set("content-type", "application/json")
+      .set("Authorization", token)
+      .send({
+        ...recipeWithInvalidData,
+      });
+    expect(response).to.have.status(StatusCodes.BAD_REQUEST);
+    expect(response.body.errors.length).to.be.at.least(5);
+    expect(response.body.errors[0].msg).to.have.string("Invalid value");
   });
 });
