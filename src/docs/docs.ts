9427c5558eb7579cbfd750040478754871b4e35b
Marcin Matoga <marcin.matoga.poznan@gmail.com>
2021-07-22 08:52:43 +0200

    Add documentation

--- src/docs/docs.ts
+++ src/docs/docs.ts
@@ -2,12 +2,11 @@ import { basicInfo } from "./basicInfo";
 import { servers } from "./servers";
 import { tags } from "./tags";
 import { components } from "./components";
-import { register } from "./user/register";
-
+import { routes } from "./routes";
 export default {
   ...basicInfo,
   ...servers,
   ...tags,
   ...components,
-  ...register,
+  ...routes,
 };
--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,5 +1,32 @@
 import { Document, Schema, model } from "mongoose";
 
+/**
+ * @swagger
+ * tags:
+ * - name: User
+ *   description: "User model"
+ * definitions:
+ *   User:
+ *     type: object
+ *     properties:
+ *       email:
+ *          type: string
+ *          description: User email
+ *          example: some@example.com
+ *       password:
+ *          type: string
+ *          description: Strong password - min 8 chars, 1 uppercase, 1 lowercase, 1 sign and 1 number
+ *          example: Password1!
+ *       isAdmin:
+ *         type: boolean
+ *         description: Defines if user is admin. Default false
+ *         example: false
+ *     required:
+ *       - email
+ *       - password
+ *
+ */
+
 export interface User {
   email: string;
   passwordHash: string;
