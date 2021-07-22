4091008ded64d5b49fc61723c2bd804a306c180a
Marcin Matoga <marcin.matoga.poznan@gmail.com>
2021-07-22 20:53:17 +0200

    Change documentation

--- src/routes/image.routes.ts
+++ src/routes/image.routes.ts
@@ -10,28 +10,34 @@ const router = express.Router();
 
 /**
  * @swagger
- * /auth/register:
- *   post:
+ * /image/:id:
+ *   get:
+ *     security:
+ *       - Bearer: []
  *     tags:
- *       - register
- *     description: Account creation
+ *       - image
+ *     description: Read all recipe images
  *     produces:
  *       - application/json
  *     parameters:
- *       - in: body
- *         name: user
+ *       - in: path
+ *         name: id
  *         schema:
- *           $ref: '#components/schemas/User'
+ *           type: string
+ *           example: 60f7ea20cf60ae0004307aa2
+ *         description: Id of post
  *
  *     responses:
  *       200:
- *         description: User created successfully
+ *         description: Found images
  *         content:
  *           application/json:
  *             schema:
- *               $ref: '#components/schemas/UserDocument'
+ *               $ref: '#components/schemas/ImageDocument'
  *       400:
- *         description: Account with this email already exists.
+ *         description: No images found
+ *       401:
+ *         description: Unauthorized
  */
 
 router.get(
@@ -48,25 +54,25 @@ router.get(
  *     security:
  *       - Bearer: []
  *     tags:
- *       - addImage
+ *       - image
  *     description: Add image
  *     parameters:
  *       - in: path
  *         name: id
  *         schema:
  *           type: string
- *           description: Id of post
  *           example: 60f7ea20cf60ae0004307aa2
+ *         description: Id of post
  *       - in: form-data
  *         name: image
  *         schema:
  *           type: Buffer
- *           description: Image
  *           example:  |
  *             <Buffer 89 50 4e 47 0d 0a 1a 0a 00 00 00 0d 49 48 44
  *             52 00 00 04 18 00 00 00 ca 08 06 00 00 00 0f 58 f3 5e
  *             00 00 18 86 69 43 43 50 49 43 43 20 50 72 6f 66 69
  *             ... 115777 more bytes>
+ *         description: Image buffer
  *
  *     responses:
  *       201:
