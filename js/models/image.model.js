a64c9478b91487be307736ad9bd1969359d5b441
Marcin Matoga <marcin.matoga.poznan@gmail.com>
2021-07-26 10:04:32 +0200

    Heroku

--- js/models/image.model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Image = void 0;
-const mongoose_1 = require("mongoose");
-const imageSchema = new mongoose_1.Schema({
-    recipeId: { type: String, required: true },
-    image: { type: Buffer, required: true },
-});
-exports.Image = mongoose_1.model("image", imageSchema);
