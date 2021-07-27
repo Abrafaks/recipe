6c7aa2e7d3f1a800f6e858ab9926612a74630647
Marcin Matoga <marcin.matoga.poznan@gmail.com>
2021-07-27 10:19:41 +0200

    Add spacebar

--- src/controllers/recipe.controller.ts
+++ src/controllers/recipe.controller.ts
@@ -61,6 +61,7 @@ export class RecipeController {
         matchedData(req);
       const { _id, isAdmin } = req.user!;
       let result: boolean;
+
       if (isAdmin) {
         result = await recipeService.updateRecipe(id, null, {
           title,
